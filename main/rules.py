0b6375737ab5365e65955dee24622ff7e367c5ed
prajnamort <prajnamort@gmail.com>
2017-08-18 13:23:34 +0800

    SubmitDetail rules added.

--- main/rules.py
+++ main/rules.py
@@ -9,7 +9,12 @@ can_access_unreleased_problems = has_faculty_privilege
 def can_access_problem(user, problem):
     return problem.released or can_access_unreleased_problems(user)
 
+@rules.predicate
+def can_view_submit(user, submit):
+    return has_faculty_privilege(user) or submit.user == user
+
 
 rules.add_rule('is_faculty', is_faculty)
 rules.add_rule('can_access_unreleased_problems', can_access_unreleased_problems)
 rules.add_rule('can_access_problem', can_access_problem)
+rules.add_rule('can_view_submit', can_view_submit)
--- main/views/submit.py
+++ main/views/submit.py
@@ -1,5 +1,8 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import PermissionDenied
+
+from rules import test_rule
 
 from main.models import Submit
 from main.serializers import SubmitSerializer
@@ -15,3 +18,7 @@ class SubmitDetail(generics.RetrieveAPIView):
     queryset = Submit.objects.all()
     serializer_class = SubmitSerializer
     permission_classes = (IsAuthenticated,)
+
+    def check_object_permissions(self, request, submit):
+        if not test_rule('can_view_submit', request.user, submit):
+            raise PermissionDenied
