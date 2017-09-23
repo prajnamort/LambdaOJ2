66310d6ea4bb3b924e63718b7b607da8a1178e43
prajnamort <prajnamort@gmail.com>
2017-09-23 18:18:34 +0800

    logic error fixed. This closes #37.

--- main/utils/compare_utils.py
+++ main/utils/compare_utils.py
@@ -10,23 +10,17 @@ def get_compare_func(compare_file_name):
 
 
 def validate_compare_file(value):
-    with open('/tmp/tmp_compare_file.py', 'wb') as fc,\
-         open('/tmp/tmp_input_sample.txt', 'w') as f1,\
-         open('/tmp/tmp_output_sample.txt', 'w') as f2:
+    with open('/tmp/tmp_compare_file.py', 'wb') as fc:
         fc.write(value.file.read())
-        f1.write('1\n')
-        f2.write('1\n')
+
     def _cleanup():
         try:
             os.remove(fc.name)
-            os.remove(f1.name)
-            os.remove(f2.name)
         except:
             pass
+
     try:
-        compare_func = get_compare_func(fc.name)
-        result = compare_func(f1.name, f2.name)
-        assert isinstance(result, bool), 'Compare 函数返回值不是 bool 类型'
+        get_compare_func(fc.name)
     except Exception as e:
         _cleanup()
         message = str(e) or 'Compare 函数文件错误，请检查语法和逻辑'
